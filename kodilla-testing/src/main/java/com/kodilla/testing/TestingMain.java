e1ca1208da1fe2f61b806897cb347b8253dccc07
pzacharzewska <pzacharzewska@gmail.com>
2020-04-05 14:39:57 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+
+        }
+        else{
+            System.out.println("Error!");
+        }
+
+        System.out.println("Pierwszy test jednostkowy");
+
+        Calculator calculator = new Calculator();
+        int result1 = calculator.add(78,23);
+        int result2 = calculator.subtract(78,23);
+
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }
