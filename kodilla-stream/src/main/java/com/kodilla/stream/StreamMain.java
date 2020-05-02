d2f69b118d11761231eef8ffe3cf610397bb1087
pzacharzewska <pzacharzewska@gmail.com>
2020-05-02 21:02:51 +0200

    7.2 version1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 
@@ -24,5 +25,9 @@ public class StreamMain {
         poemBeautifier.beautify("Minka", "", PoemBeautifier::addRandomLetterToTheEnd);
         poemBeautifier.beautify("Minka", " to mój kot.", PoemBeautifier::addRandomLetterAtTheBeginning);
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
     }
+
 }
